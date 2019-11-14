9d87089256696eb6a4cf53626e3c5810ce2d44d0
Sebastian <secastroal@gmail.com>
2019-11-14 14:11:15 +0100

    Initial push

---
+++ Stan/ram.stan
@@ -0,0 +1,109 @@
+data {
+  int<lower=0> nr; // number of rows
+  int<lower=0> n;  // number of subjects
+  int<lower=0> p;  // number of items
+  int<lower=0> K;  // number of categories per items
+  
+  int Y[nr];
+  real TP[nr];           // time point
+  int<lower=0> X1[nr];   // patient indicator
+  int<lower=0> item[nr]; // item indicator
+
+}
+parameters {
+  // Parameters
+  vector[n] mu_i;          // Random intercept
+  vector[n] beta_i;        // Random slope for TP
+  vector[n] r_i;           // Random amplitude
+  vector[n] phi_i;         // Random phase shift
+  real<lower = 0> sigma_2; // Residual variance
+  vector[p - 1] lambda_f;  // Location parameters free
+  vector[K - 2] delta_f;   // Tresholds parameters free
+  vector[nr] theta;        // Theta of person i at time t
+  
+  // Hyper parameters
+  real mu_gamma0;         // Mean mu
+  real<lower=0> v_gamma0; // Var mu
+  real mu_gamma1;         // Mean beta  
+  real<lower=0> v_gamma1; // Var beta
+  real mu_gamma2;         // Mean r
+  real<lower=0> v_gamma2; // Var r
+  real mu_gamma3;         // Mean phi
+  real<lower=0> v_gamma3; // Var phi
+}
+transformed parameters {
+  real<lower=0> s_gamma0; // Sd mu
+  real<lower=0> s_gamma1; // Sd beta
+  real<lower=0> s_gamma2; // Sd r
+  real<lower=0> s_gamma3; // Sd phi
+  real<lower=0> sigma;    // Sd residual variance
+  vector[p] lambda;       // Location parameters
+  vector[K - 1] delta;    // Threshold parameters
+  vector[K] deltasum; // Cumulative sum threshold parameters
+  vector[nr] theta_i;     // Predicted Theta of person i at time t
+  vector[K] args [nr];        // Create vector argument for softmax function
+  vector[K] probs [nr];   // Output vector of the softmax function 
+  
+  // Compute standard deviations
+  s_gamma0 = sqrt(v_gamma0);
+  s_gamma1 = sqrt(v_gamma1);
+  s_gamma2 = sqrt(v_gamma2);
+  s_gamma3 = sqrt(v_gamma3);
+  sigma    = sqrt(sigma_2);
+  
+  // Constrain sum of location and threshold parameters to 0
+  for (i in 1:(p-1)) {
+    lambda[i] = lambda_f[i]; 
+  }
+  lambda[p] = -sum(lambda_f);
+  
+  for (j in 1:(K-2)) {
+    delta[j] = delta_f[j]; 
+  }
+  delta[K-1] = -sum(delta_f);
+  
+  // Compute cumulative sum of threshold parameters
+  deltasum[1] = 0.0;
+  deltasum[2:K] = cumulative_sum(delta);
+  
+  // Compute intraindividual means of theta
+  
+  for (rr in 1:nr) {
+    theta_i[rr] = mu_i[X1[rr]] + beta_i[X1[rr]] * TP[rr] + r_i[X1[rr]] * (cos(((2*pi())/7))*TP[rr] + phi_i[X1[rr]]);
+  }
+  
+  // Compute probabilities to input in likelihood
+  for (s in 1:nr) {
+    for (y in 0:(K - 1)) args[s, y + 1] = (y * (theta[s] - lambda[item[s]]) - deltasum[y + 1]);
+    probs[s] = softmax(args[s]);
+  }
+}
+model {
+  // Define priors
+  mu_i ~ normal(mu_gamma0, s_gamma0);          
+  beta_i ~ normal(mu_gamma1, s_gamma1);
+  r_i ~ normal(mu_gamma2, s_gamma2);           
+  phi_i ~ normal(mu_gamma3, s_gamma3);
+  sigma_2 ~ inv_gamma(1, 1) ;// Residual variance
+  lambda_f ~ normal(0, 1);  // Location parameters free
+  delta_f ~ normal(0, 1);   // Tresholds parameters free
+  theta ~ normal(theta_i, sigma);        // Theta of person i at time t
+  
+  // Hyper parameters
+  
+  // Define Hyperpriors
+  mu_gamma0 ~ normal(0, 1);
+  v_gamma0 ~ inv_gamma(1, 1);
+  mu_gamma1 ~ normal(0, 1);   
+  v_gamma1 ~ inv_gamma(1, 1);
+  mu_gamma2 ~ normal(0, 1);  
+  v_gamma2 ~ inv_gamma(1, 1);
+  mu_gamma3 ~ normal(0, 1);
+  v_gamma3 ~ inv_gamma(1, 1); 
+  
+  // Likelihood
+  for(rr in 1:nr){
+    Y[rr] ~ categorical(probs[rr]);
+  }
+}
+
---
+++ main.R
@@ -0,0 +1,250 @@
+# In this document I try out some of the longitudinal IRT models.
+# The next sections are included:
+# 0. Prepare enviroment
+# 1. Vandemeulebroecke et al (2017)
+# 2. Ram et al (2005)
+
+
+# 0. Prepare enviroment ----
+rm(list = ls())
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+# This function is to compute the probabilities of the Graded Response Model, which will be quite useful.
+P.GRM <- function(C, IP, theta)
+{
+  N         <- length(theta)
+  I         <- nrow(IP)
+  alpha     <- IP[, ncol(IP)]
+  betas     <- IP[, -ncol(IP)]
+  res.cum   <- array(NA, c(N, I, C))
+  for (i in 1:I)
+  {
+    for (c in 1:C)
+    {
+      arg              <- alpha[i] * (theta - betas[i, c])
+      res.cum[ , i, c] <- exp(arg) / (1 + exp(arg))
+    }
+  }
+  res.cum <- array(c(matrix(1, N, I), res.cum, matrix(0, N, I)), dim = c(N, I, C + 2))
+  res     <- array(NA, c(N, I, C + 1))
+  for (c in 1:(C + 1)) res[, , c] <- res.cum[, , c] - res.cum[, , c + 1]
+  return(res)
+}
+
+# 1. Vandemeulebroecke et al (2017) ----
+# We will generate date based on the model proposed by Vandemeulebroecke et al (2017), 
+# and fit the model to the data. 
+
+# Simulating data
+# For this, we need: 
+#   The parameters of the model,
+#   A dataset of covariates,
+#   A dataset that includes responses of persons to items in long format.
+
+set.seed(123)
+# Let's define the conditions of the data
+n  <- 100 # Number of subjects.
+p  <- 5   # Number of items.
+K  <- 5   # Number of categories per items.
+nC <- 2   # Number of covariates.
+nt <- 5  # Number of time points. #! not necessarily the same for all the persons
+
+# Now, we generate the thetas given the longitudinal model proposed by Vandemeulebroecke
+
+gamma0    <- rnorm(n)                             # Random intercept, which varies over persons.
+gammastar <- rnorm(n, 0, 0.3)                     # Random intercept of the random slope, which varies over persons.
+beta      <- sample((-3:3)/50, nC, replace = TRUE) # Coefficients of the covariates. 
+Z         <- replicate(nC, rnorm(n))              # Generate nC standardized covariates. 
+
+gamma1    <- gammastar + Z %*% beta               # Compute random slope.
+
+# Create matrix to store thetas over time
+theta <- matrix(NA, n, nt)
+for (i in 1:nt) {
+  theta[, i] <- gamma0 + gamma1 * i 
+}
+rm(i)
+
+theta <- reshape(as.data.frame(theta), 
+                 varying       = list(1:nt), 
+                 new.row.names = 1:(n * nt),
+                 direction     = "long") # Turn theta matrix into long format.
+theta <- theta[order(theta[, 3]), c(3, 1, 2)]                                    # Order theta matrix by id.    
+
+# Generate GRM responses given the generated thetas.
+
+# Create item parameters
+alpha      <- runif(p, 0.5 , 2)    # Discrimination parameters.
+delta      <- matrix(NA, p, K - 1) # Matrix to store difficulty parameters.
+delta[, 1] <- rnorm(p)
+for (i in 2:(K - 1)){
+  delta[, i] <- delta[, i - 1] + runif(1, 0.4, 0.9)
+}
+rm(i)
+
+IP          <- cbind(delta, alpha) # Matrix of item parameters to input in P.GRM
+
+probs.array <- P.GRM(K - 1, IP, theta[, 3])
+responses   <- apply(probs.array, 1:2, function(vec) {which( rmultinom(1, 1, vec) == 1) - 1 })
+rm(probs.array, delta, alpha)
+
+# Restructure data to add id, time, and item factors.
+simdata <- data.frame(theta[, 1:2], responses)
+
+simdatalong <- reshape(simdata, 
+                       idvar         = c("id", "time"),
+                       varying       = list(3:7),
+                       new.row.names = 1:(n * nt * p),
+                       direction     = "long") 
+
+names(simdatalong) <- c("id", "item", "X")
+time               <- rep(1:nt, n * p)
+simdatalong        <- data.frame(simdatalong, time)
+rm(time, simdata, responses)
+
+simdatalong <- simdatalong[order(simdatalong[, 1], simdatalong[, 4]), c(1, 4, 2, 3)] # order data by id and time
+
+nr <- nrow(simdatalong) # Number of observed responses. Might be fewer than p * nt * i given missing values.
+
+# Run Vandemeulebroecke's model
+
+# Prepare data for stan
+
+standata <- list(nr           = nr,
+                 n            = n,
+                 p            = p,
+                 K            = K,
+                 nC           = nC,
+                 Y            = simdatalong[, "X"] + 1,
+                 TP           = simdatalong[, "time"],
+                 X1           = simdatalong[, "id"],
+                 item         = simdatalong[, "item"],
+                 Z            = Z,
+                 m_mu_gamma1  = 0,
+                 sd_mu_gamma1 = 1,
+                 m_alpha      = 0,
+                 sd_alpha     = 1,
+                 m_kappa      = 0,
+                 sd_kappa     = 1,
+                 a_pr_gamma1  = 1,
+                 b_pr_gamma1  = 1
+                 )
+
+fit.vande <- stan(file = "Stan/vandemeulebroecke.stan", data = standata, iter = 1000, chains = 3, 
+                  thin = 5, cores = 3)
+
+traceplot(fit.vande, pars = "alpha[1]", inc_warmup = TRUE)
+
+summary(fit.vande, pars = "alpha")$summary
+
+# 2. Ram et al (2005) ----
+# LIRT model proposed by Ram et al. (2005) is based on the rating scale model. 
+# The longitudinal aspect is modeled through spectral analysis.
+
+# Simulating data
+# We will generate data given the spectral analysis model of Ram and the GRM.
+# For this, we need: 
+#   The parameters of the spectral model,
+#   The parameters of the GRM,
+
+set.seed(123)
+# Let's define the conditions of the data
+n  <- 100 # Number of subjects.
+p  <- 5   # Number of items.
+K  <- 5   # Number of categories per items.
+nt <- 56  # Number of time points. #! not necessarily the same for all the persons
+
+# Now, we generate the thetas given spectral analysis proposed by Ram
+# We need the next random parameters:
+
+mu    <- rnorm(n, -0.5, 1)  # Random intercept.
+beta  <- rnorm(n, 0, .01)   # Random linear slope, which multiplies time_it. Detrend data.
+R     <- rnorm(n, 0.2, 0.2) # Random amplitude of the cycle.
+omega <- (2 * pi) / 7       # Period of oscillattion assumed to be 7 days.
+phi   <- rnorm(n, 0.2, 0.4) # Random phase shift.
+
+# Create matrix to store thetas over time
+theta <- matrix(NA, n, nt)
+for (i in 1:nt) {
+  theta[, i] <- mu + beta * (i - 1) + R * (cos((omega * i) + phi)) + rnorm(1, 0, 0.5) 
+}
+rm(i)
+
+theta <- reshape(as.data.frame(theta), 
+                 varying       = list(1:nt), 
+                 new.row.names = 1:(n * nt),
+                 direction     = "long") # Turn theta matrix into long format.
+theta <- theta[order(theta[, 3]), c(3, 1, 2)]                                    # Order theta matrix by id.    
+
+# Generate GRM responses given the generated thetas.
+
+# Create item parameters
+alpha      <- rep(1, p)            # Discrimination parameters.
+delta      <- matrix(NA, p, K - 1) # Matrix to store difficulty parameters.
+delta[, 1] <- rnorm(p)
+for (i in 2:(K - 1)){
+  delta[, i] <- delta[, i - 1] + runif(1, 0.4, 0.9)
+}
+rm(i)
+
+IP          <- cbind(delta, alpha) # Matrix of item parameters to input in P.GRM
+
+probs.array <- P.GRM(K - 1, IP, theta[, 3])
+responses   <- apply(probs.array, 1:2, function(vec) {which( rmultinom(1, 1, vec) == 1) - 1 })
+rm(probs.array, delta, alpha)
+
+# Restructure data to add id, time, and item factors.
+simdata <- data.frame(theta[, 1:2], responses)
+
+simdatalong <- reshape(simdata, 
+                       idvar         = c("id", "time"),
+                       varying       = list(3:7),
+                       new.row.names = 1:(n * nt * p),
+                       direction     = "long") 
+
+names(simdatalong) <- c("id", "item", "X")
+time               <- rep(1:nt, n * p)
+simdatalong        <- data.frame(simdatalong, time)
+rm(time, simdata, responses)
+
+simdatalong <- simdatalong[order(simdatalong[, 1], simdatalong[, 4]), c(1, 4, 2, 3)] # order data by id and time
+
+nr <- nrow(simdatalong) # Number of observed responses.
+
+# Run Ram's model
+
+standata <- list(nr           = nr,
+                 n            = n,
+                 p            = p,
+                 K            = K,
+                 Y            = simdatalong[, "X"] + 1,
+                 TP           = simdatalong[, "time"],
+                 X1           = simdatalong[, "id"],
+                 item         = simdatalong[, "item"]
+                 )
+
+fit.ram <- stan(file = "Stan/ram.stan", 
+                data = standata,
+                pars = c("lambda", 
+                         "delta",
+                         "mu_gamma0",
+                         "v_gamma0",
+                         "mu_gamma1",
+                         "v_gamma1",
+                         "mu_gamma2",
+                         "v_gamma2",
+                         "mu_gamma3",
+                         "v_gamma3",
+                         "sigma_2"),
+                iter   = 1000, 
+                chains = 3, 
+                thin   = 5, 
+                cores  = 3)
+
+traceplot(fit.ram, pars = "v_gamma2", inc_warmup = TRUE)
+
+summary(fit.ram, pars = "delta")$summary
+
+
