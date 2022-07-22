d2972dcc088c142d7af2ccf09fd662d3db3bddc8
Sebastian <secastroal@gmail.com>
2022-07-22 10:33:17 +0200

    Update plots for the manuscript.

--- ManuscriptPlots.R
+++ ManuscriptPlots.R
@@ -9,24 +9,38 @@ library(splines)
 
 # ICC of PCM ----
 
-probs <- matrix(NA, 200, 3)
-for (yy in 0:2) {
-  probs[, yy + 1] <- P.GPCM(yy, alpha =  1, delta = 0, taus = c(-1, 1), 
-                        theta = seq(-3, 3, length.out = 200), M = 2)
+thresholds <- c(-1.42, -0.88, -0.09, 0.51)
+probs <- matrix(NA, 200, length(thresholds) + 1)
+for (yy in 0:length(thresholds)) {
+  probs[, yy + 1] <- P.GPCM(yy, alpha =  1, delta = mean(thresholds), 
+                            taus = thresholds - mean(thresholds), 
+                        theta = seq(-3, 3, length.out = 200), 
+                        M = length(thresholds))
+}
+rm(yy)
+
+intersections <- matrix(NA, length(thresholds), length(thresholds) + 1)
+for (yy in 0:length(thresholds)) {
+  intersections[, yy + 1] <- P.GPCM(yy, alpha =  1, delta = mean(thresholds), 
+                            taus = thresholds - mean(thresholds), 
+                            theta = thresholds, 
+                            M = length(thresholds))
 }
 rm(yy)
 
 pdf(file = "Figures/ICC-example.pdf", heigh = 4)
 par(mar = c(4, 4.5, 2, 2) + 0.1)
 matplot(seq(-3, 3, length.out = 200), probs, las = 1,
-        ylim = c(0, 1), lty = 1:3, type = "l", col = gray((2:0)/3),
+        ylim = c(0, 1), lty = 1:5, type = "l", col = gray((2)/5),
         lwd = 2, xlab = expression(theta), cex.lab = 1.2, 
         ylab = expression(paste("P(X"[i], " = x |", theta, ")")))
-legend("bottomright", legend = c("x = 0", "x = 1", "x = 2"),
-       col = gray((2:0)/3), lty = 1:3, lwd = 2, seg.len = 3.5, bg = "white")
+segments(x0 = thresholds, y0 = -0.05, y1 = intersections, 
+         col = gray(1/2), lty = 3)
+legend("bottomright", legend = c("x = 0", "x = 1", "x = 2", "x = 3", "x = 4"),
+       col = gray(2/5), lty = 1:5, lwd = 2, seg.len = 3.75, bg = "white")
 dev.off()
 
-rm(probs)
+rm(probs, intersections, thresholds)
 
 # B-splines ----
 set.seed(1234)
