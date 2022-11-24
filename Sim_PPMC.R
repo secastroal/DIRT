1c6956785d618b188a243f143d38ecea980910b9
Sebastian <secastroal@gmail.com>
2022-11-24 18:45:24 +0100

    New file for simulation of PPMC methods.

---
+++ Sim_PPMC.R
@@ -0,0 +1,15 @@
+# Simulation study to test the PPMC methods for the TV-DPCM
+
+# Data generating models
+
+# TV-DPCM (control)
+# TV-MDPCM (maybe low vs high correlation)
+# TV-GPCM (1/3 or 2/3 different)
+# TV-DPCM-IPD (1/3 or 2/3 different)
+# TV-DPCM-Meaning (1/3 or 2/3 different)
+
+
+# Conditions
+# Time series length nT <- 300
+# Either 3 or 6 items (preferably 6)
+
